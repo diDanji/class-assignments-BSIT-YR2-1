ad36730d5e7468ed0d8146bbd96af4fefcb6e504
tony dilli <dilli23tonny@gmail.com>
2025-09-09 10:49:26 +0300

    working on objects

---
+++ objects.py
@@ -0,0 +1,17 @@
+my_dog = Dog("Buddy", "Golden Retriever")
+print(my_dog.name)  # Output: Buddy
+print(my_dog.bark()) # Output: Buddy barks!
+
+#In Python, an instance is a specific object created from a class.
+#A class is like a blueprint (for example, Dog).
+#An instance is a real object made from that blueprint (for example, my_dog = Dog("Buddy", "Golden Retriever")).
+#So, my_dog is an instance of the Dog class.
+#You can create many instances (objects) from the same class, each with its own data.
+
+hospital_patient = Patient("Allan", "Amos")
+shopping_cart = cart("groceries", "detergents")
+hostel_housing = housing("single", "double")
+retail_product = product("laptop", "smartphone")
+university_course = course("math", "science")
+
+
